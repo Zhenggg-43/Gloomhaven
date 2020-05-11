fe81e157edff20728fc8e8d456ef69c0aadf3c87
宗宜 陳 <ian03170317@gmail.com>
2020-05-11 15:21:45 +0800

    5/11 03:21 pm

--- Gloomhaven/Gloomhaven/gamemanager.cpp
+++ Gloomhaven/Gloomhaven/gamemanager.cpp
@@ -2,14 +2,18 @@
 #include"loadgame.h"
 #include"whether_play.h"
 #include"play_game.h"
+#include"menu.h"
 bool gameloop()
 {
+	/////////////////////////
 	if (!whether_play())
 		return false;
-
+	/////////////////////////
 	loadgame();
-
+	/////////////////////////
+	menu();
+	/////////////////////////
 	play_game();
-
+	/////////////////////////
 	return true;
 }
---
+++ Gloomhaven/Gloomhaven/menu.cpp
@@ -0,0 +1,8 @@
+#include"menu.h"
+#include<iostream>
+extern void menu()
+{
+	std::cout << "你輸入了角色數量你真棒!!" << std::endl;
+	std::cout << "你輸入了出場角色跟技能卡你真棒!!" << std::endl;
+	std::cout << "你輸入了地圖你真棒!!" << std::endl;
+}
